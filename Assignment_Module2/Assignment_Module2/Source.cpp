3c47aa6e00ea464e1a3c63acbc2fcabe7bcae9ba
Mosemb <mosejava@gmail.com>
2017-06-07 01:27:42 +0200

    Another commit

---
+++ Assignment_Module2/Assignment_Module2/Source.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** arg) {
+
+	// Powers function
+
+	{
+		
+
+	int base = 2;
+	int tot = 1;
+
+
+
+	for (int iep = 1; iep < 9; iep++)
+	{
+		tot = tot*base;
+		
+	}
+
+	
+
+	{
+		cout << tot << endl;
+	}
+
+}
+	
+
+	return 0;
+
+
+}
