7c8086ceb98ef50c4e43551cc088b896ad2dc175
mosemb <mosejava@gmail.com>
2017-07-14 15:25:28 +0200

    Git Linux

--- LinuxRealTest/main.cpp
+++ LinuxRealTest/main.cpp
@@ -11,5 +11,11 @@ int main() {
     A a = {1, 2 , 3};
     printf("System print Struct A \n ia:%d\n ib:%d\n ic:%d\n ", a.ia, a.ib, a.ic);
 
+    // if we had a pointer then
+    A *pa = &a;
+    printf("System print Struct A with Pointers \n ia:%d\n ib:%d\n ic:%d\n ", pa->ia, pa->ib, pa->ic);
+    int m;
+
+
     return 0;
 }
